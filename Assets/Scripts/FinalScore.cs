df2588a0454e70f81d2878a4e526553a101ce3d9
Beermu93 <tomi.bermu01@gmail.com>
2025-06-29 16:37:42 -0300

    Fixed LoseScreen animation

--- Assets/Scripts/FinalScore.cs
+++ Assets/Scripts/FinalScore.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class FinalScore : MonoBehaviour
 {
@@ -11,12 +12,19 @@ public class FinalScore : MonoBehaviour
     public int finalScore = 0;
     public int highScore = 0;
 
-    // Start is called before the first frame update
+    [SerializeField] private RawImage _img;
+    [SerializeField] private float _x, _y;
+
     void Start()
     {
+        gameObject.GetComponent<Canvas>().worldCamera = Camera.main;
         LoadScoreAsync();
         finalScore = Score.scorePoints;
         _text.text = finalScore.ToString();
+    }
+    void Update()
+    {
+        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x, _y) * Time.unscaledDeltaTime, _img.uvRect.size);
     }
     private async void SaveScoreAsync() { await CloudSaveSystem.SaveScoreAsync(highScore); }
 
